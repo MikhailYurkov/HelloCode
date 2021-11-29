15b357a1fd688c29fa986c43241086daf654abda
Mikl Yurkov <9906205@gmail.com>
2021-11-29 20:50:40 +0300

    задача 15

--- Solution 1/Program.cs
+++ Solution 1/Program.cs
@@ -1,7 +1,9 @@
 ﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
-
+int n =
+int i = 
 int is_square(int n)
 {
     for(int i = 1; n > 0; i += 2) n -= i;
-    return !n;
+    return n;
 }
+Console.WriteLine (is_square);
